daf89d5db7b069428fb85c27ef192db2d9f0a976
1bitbool <1bitbool@gmail.com>
2023-10-02 15:19:17 +0800

    Refactor: replace dialog with react-aria component

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { useState } from "react";
 import { HiOutlineXMark as XMarkIcon } from "react-icons/hi2";
 import { HiOutlineBars3 as Bars3Icon } from "react-icons/hi2";
 import { FaGlobe } from "react-icons/fa";
-import { Dialog, Modal } from "react-aria-components";
+import { Button, Dialog, DialogTrigger, Modal } from "react-aria-components";
 
 const navigation = [
   { name: "Posts", href: "#" },
@@ -11,8 +10,6 @@ const navigation = [
 ];
 
 export default function Navigation() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
     <header className="bg-white">
       <nav
@@ -30,14 +27,63 @@ export default function Navigation() {
           </a>
         </div>
         <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
+          <DialogTrigger>
+            <Button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </Button>
+            <Modal className="lg:hidden">
+              <div className="fixed inset-0 z-10" />
+
+              <Dialog className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                {({ close }) => (
+                  <>
+                    <div className="flex items-center justify-between">
+                      <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">1BitBool</span>
+                        <img
+                          className="h-8 w-auto"
+                          src="/assets/vercel.svg"
+                          alt="1BitBool"
+                        />
+                      </a>
+                      <Button
+                        type="button"
+                        className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                        onPress={close}
+                      >
+                        <span className="sr-only">Close menu</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      </Button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                      <div className="-my-6 divide-y divide-gray-500/10">
+                        <div className="space-y-2 py-6">
+                          {navigation.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            >
+                              {item.name}
+                            </a>
+                          ))}
+                        </div>
+                        <div className="py-6">
+                          <button className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                            Language
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </Dialog>
+            </Modal>
+          </DialogTrigger>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
@@ -60,54 +106,6 @@ export default function Navigation() {
           </button>
         </div>
       </nav>
-      <Modal
-        className="lg:hidden"
-        isOpen={mobileMenuOpen}
-        onOpenChange={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10" />
-
-        <Dialog className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">1BitBool</span>
-              <img
-                className="h-8 w-auto"
-                src="/assets/vercel.svg"
-                alt="1BitBool"
-              />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="py-6">
-                <button className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                  Language
-                </button>
-              </div>
-            </div>
-          </div>
-        </Dialog>
-      </Modal>
     </header>
   );
 }
