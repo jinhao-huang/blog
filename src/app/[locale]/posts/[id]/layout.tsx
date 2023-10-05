3b55bf968873d52cdcf7233070cb79b1d7f3825a
1bitbool <1bitbool@gmail.com>
2023-10-05 21:25:33 +0800

    Change: post padding

--- src/app/[locale]/posts/[id]/layout.tsx
+++ src/app/[locale]/posts/[id]/layout.tsx
@@ -4,7 +4,7 @@ export default function PostLayout({
   children: React.ReactNode;
 }) {
   return (
-    <article className="prose dark:prose-invert mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-16 sm:py-24">
+    <article className="prose dark:prose-invert mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-24 sm:py-32">
       {children}
     </article>
   );
