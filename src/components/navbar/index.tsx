5241ef5fde50744a2cab6bdd82a6274057e0ff6f
1bitbool <1bitbool@gmail.com>
2023-10-06 21:01:41 +0800

    Change: logo link

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -17,10 +17,10 @@ export default function Navbar() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">1BitBool</span>
             <img className="h-8 w-auto" src="/assets/logo.svg" alt="1BitBool" />
-          </a>
+          </Link>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigations.map((item) => (
