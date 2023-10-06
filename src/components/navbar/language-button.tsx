3eeb9b07a37b12371b68d03263b144041df15b8e
1bitbool <1bitbool@gmail.com>
2023-10-06 16:09:28 +0800

    Change: language button icon

--- src/components/navbar/language-button.tsx
+++ src/components/navbar/language-button.tsx
@@ -6,7 +6,7 @@ import {
   Popover,
   Item,
 } from "react-aria-components";
-import { FaGlobe } from "react-icons/fa";
+import { TbWorld } from "react-icons/tb";
 import { usePathname, useRouter } from "next-intl/client";
 import { Key } from "react";
 
@@ -24,7 +24,7 @@ export default function LanguageButton({
         className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-gray-300"
       >
         <span className="sr-only">{aria_dict.switch_language}</span>
-        <FaGlobe className="h-6 w-6"></FaGlobe>
+        <TbWorld className="h-6 w-6"></TbWorld>
       </Button>
       <Popover>
         <Menu
