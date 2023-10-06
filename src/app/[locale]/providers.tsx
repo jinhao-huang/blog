74c1388f9a9d6825d05a46aabe165fe349a15720
1bitbool <1bitbool@gmail.com>
2023-10-06 14:43:44 +0800

    Add: dark color for menu

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -2,5 +2,5 @@
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
