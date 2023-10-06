3c06a46005c024d2a888a68538ca820fb976ec24
1bitbool <1bitbool@gmail.com>
2023-10-06 20:52:44 +0800

    Add: about component

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,14 @@
+import { getContent } from "@/lib/get-contents";
+import { notFound } from "next/navigation";
+import { Locale } from "@/i18n";
+import ContentBody from "@/components/content-body";
+
+export default async function About({
+  params: { locale },
+}: {
+  params: { locale: Locale };
+}) {
+  const content = await getContent(locale, "about");
+  if (!content) notFound();
+  return <ContentBody>{content.body}</ContentBody>;
+}
