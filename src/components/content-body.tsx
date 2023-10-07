3825fc201f7f7af58cb60644dbe53b790c512dc6
1bitbool <1bitbool@gmail.com>
2023-10-07 18:44:20 +0800

    Fix: content-body style

--- src/components/content-body.tsx
+++ src/components/content-body.tsx
@@ -2,7 +2,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 
 export default function ContentBody({ children }: { children: string }) {
   return (
-    <article className="prose dark:prose-invert">
+    <article className="prose dark:prose-invert mx-auto">
       <MDXRemote
         source={children}
         options={{
